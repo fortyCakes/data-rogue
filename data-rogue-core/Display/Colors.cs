09840fffa51bce1d8a65934f8526a0cda39da3ca
fortyCakes <alex.j.stuart@gmail.com>
2017-02-21 14:16:00 +0000

    Add core project to source control folder

---
+++ data-rogue-core/Display/Colors.cs
@@ -0,0 +1,19 @@
+﻿using RLNET;
+
+namespace data_rogue_core.Display
+{
+    public class Colors
+    {
+        public static RLColor FloorBackground = RLColor.Black;
+        public static RLColor Floor = Swatch.AlternateDarkest;
+        public static RLColor FloorBackgroundFov = Swatch.DbDark;
+        public static RLColor FloorFov = Swatch.Alternate;
+
+        public static RLColor WallBackground = Swatch.SecondaryDarkest;
+        public static RLColor Wall = Swatch.Secondary;
+        public static RLColor WallBackgroundFov = Swatch.SecondaryDarker;
+        public static RLColor WallFov = Swatch.SecondaryLighter;
+
+        public static RLColor TextHeading = Swatch.DbLight;
+    }
+}
