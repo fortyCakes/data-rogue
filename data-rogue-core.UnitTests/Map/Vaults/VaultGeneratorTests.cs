5240ef982101ff4e3fb7b267b97cbd2851d5c743
fortyCakes <alex.j.stuart@gmail.com>
2017-02-22 17:49:10 +0000

    Tests

---
+++ data-rogue-core.UnitTests/Map/Vaults/VaultGeneratorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using data_rogue_core.Map.Vaults;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using RogueSharp.Random;
+
+namespace data_rogue_core.UnitTests.Map.Vaults
+{
+    [TestFixture]
+    public class VaultDataParserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void GetVault_ReturnsDungeonMap()
+        {
+            var parser = new VaultDataParser();
+            var testJson = GetTestJson();
+
+            var action = new Action(() => parser.ParseVault(testJson));
+
+            action.ShouldNotThrow();
+        }
+
+        private string GetTestJson()
+        {
+            return @"{""map"":""
+......
+......
+~~~~~~
+......
+......
+""}";
+        }
+    }
+}
