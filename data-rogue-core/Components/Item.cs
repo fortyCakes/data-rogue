fd28ff09dd43fac616dfd08c69f60f0f7cb804c4
fortyCakes <alex.j.stuart@gmail.com>
2019-03-14 17:58:39 +0000

    item changes

--- data-rogue-core/Components/Item.cs
+++ data-rogue-core/Components/Item.cs
@@ -5,7 +5,6 @@ namespace data_rogue_core.Components
     public class Item : IEntityComponent
     {
         public string UseScript;
-        public int Power = 0;
         public int ItemLevel = 0;
     }
 }
