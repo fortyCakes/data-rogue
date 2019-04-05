56dcddfc7e785901cdbce85d17d04bd9adff8725
fortyCakes <alex.j.stuart@gmail.com>
2019-04-05 16:45:44 +0100

    Add failing tests for new event system behaviour

--- data-rogue-core.UnitTests/EventSystem/EventSystemTests.cs
+++ data-rogue-core.UnitTests/EventSystem/EventSystemTests.cs
@@ -79,10 +79,101 @@ namespace data_rogue_core.UnitTests.EventSystem
             rule1.DidNotReceiveWithAnyArgs().Apply(Arg.Any<EventType>(), null, null);
         }
 
-        private static IEventRule GetTestRule(bool ruleSucceeds = true, uint ruleOrder = 0)
+        [Test]
+        public void Try_ExecutesRuleTypesInOrder()
+        {
+            var rule1 = GetTestRule(ruleType: EventRuleType.BeforeEvent, ruleOrder: 1);
+            var rule2 = GetTestRule(ruleType: EventRuleType.EventResolution, ruleOrder: 2);
+            var rule3 = GetTestRule(ruleType: EventRuleType.AfterSuccess, ruleOrder: 2);
+            var rule4 = GetTestRule(ruleType: EventRuleType.Finally, ruleOrder: 2);
+
+            var ruleOnFail = GetTestRule(ruleType: EventRuleType.AfterFailure, ruleOrder: 2);
+
+            _eventSystem.RegisterRules(rule1, rule2, rule3, rule4, ruleOnFail);
+
+            _eventSystem.Try(EventType.Action, _entity, null);
+
+            Received.InOrder(() =>
+            {
+                rule1.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+                rule2.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+                rule3.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+                rule4.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+            });
+
+            ruleOnFail.DidNotReceiveWithAnyArgs().Apply(Arg.Any<EventType>(), null, null);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void Try_WhateverTheEventSuccessIs_RunsFinally(bool ruleSuceeds)
+        {
+            var beforeRule = GetTestRule(ruleType: EventRuleType.BeforeEvent, ruleSucceeds: ruleSuceeds);
+            var finallyRule = GetTestRule(ruleType: EventRuleType.Finally, ruleSucceeds: ruleSuceeds);
+
+            _eventSystem.RegisterRules(beforeRule, finallyRule);
+
+            _eventSystem.Try(EventType.Action, _entity, null);
+
+            Received.InOrder(() =>
+            {
+                beforeRule.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+                finallyRule.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+            });
+        }
+
+        [Test]
+        public void Try_BeforeEventPasses_RunsAfterSuccess_NotAfterFailure()
+        {
+            var rule1 = GetTestRule(ruleType: EventRuleType.BeforeEvent, ruleOrder: 1, ruleSucceeds: true);
+            var rule2 = GetTestRule(ruleType: EventRuleType.EventResolution, ruleOrder: 2);
+            var rule3 = GetTestRule(ruleType: EventRuleType.AfterSuccess, ruleOrder: 2);
+            var rule4 = GetTestRule(ruleType: EventRuleType.Finally, ruleOrder: 2);
+
+            var ruleOnFail = GetTestRule(ruleType: EventRuleType.AfterFailure, ruleOrder: 2);
+
+            _eventSystem.RegisterRules(rule1, rule2, rule3, rule4, ruleOnFail);
+
+            _eventSystem.Try(EventType.Action, _entity, null);
+
+            Received.InOrder(() =>
+            {
+                rule1.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+                rule3.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+            });
+
+            ruleOnFail.DidNotReceiveWithAnyArgs().Apply(Arg.Any<EventType>(), null, null);
+        }
+
+        [Test]
+        public void Try_BeforeEventFails_RunsAfterFailure_NotAfterSuccess()
+        {
+            var rule1 = GetTestRule(ruleType: EventRuleType.BeforeEvent, ruleOrder: 1, ruleSucceeds: false);
+            var rule2 = GetTestRule(ruleType: EventRuleType.EventResolution, ruleOrder: 2);
+            var rule3 = GetTestRule(ruleType: EventRuleType.AfterSuccess, ruleOrder: 2);
+            var rule4 = GetTestRule(ruleType: EventRuleType.Finally, ruleOrder: 2);
+
+            var ruleOnFail = GetTestRule(ruleType: EventRuleType.AfterFailure, ruleOrder: 2);
+
+            _eventSystem.RegisterRules(rule1, rule2, rule3, rule4, ruleOnFail);
+
+            _eventSystem.Try(EventType.Action, _entity, null);
+
+            Received.InOrder(() =>
+            {
+                rule1.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+                ruleOnFail.Apply(Arg.Any<EventType>(), Arg.Any<IEntity>(), Arg.Any<object>());
+            });
+
+            rule3.DidNotReceiveWithAnyArgs().Apply(Arg.Any<EventType>(), null, null);
+        }
+
+        private static IEventRule GetTestRule(bool ruleSucceeds = true, uint ruleOrder = 0, EventRuleType ruleType = EventRuleType.BeforeEvent)
         {
             var rule = Substitute.For<IEventRule>();
             rule.Apply(Arg.Any<EventType>(), null, null).ReturnsForAnyArgs(ruleSucceeds);
+            rule.RuleType.ReturnsForAnyArgs(ruleType);
             rule.EventTypes.ReturnsForAnyArgs(new EventTypeList { EventType.Action });
             rule.RuleOrder.ReturnsForAnyArgs(ruleOrder);
             return rule;
