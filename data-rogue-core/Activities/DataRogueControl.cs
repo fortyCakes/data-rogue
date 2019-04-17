4b306be93a3226c5ddc6a0e0483eda14b163394f
fortyCakes <alex.j.stuart@gmail.com>
2019-04-17 18:10:04 +0100

    Add control interface and abstract class

---
+++ data-rogue-core/Activities/DataRogueControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace data_rogue_core.Activities
+{
+    public abstract class DataRogueControl : IDataRogueControl
+    {
+        public bool IsFocused { get; set; }
+        public bool IsPressed { get; set; }
+        public Rectangle Position { get; set; } = new Rectangle();
+
+        public event PositionEventHandler Click;
+    }
+}
---
+++ data-rogue-core/Activities/IDataRogueControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace data_rogue_core.Activities
+{
+    public interface IDataRogueControl
+    {
+        bool IsFocused { get; set; }
+        bool IsPressed { get; set; }
+        Rectangle Position { get; set; }
+
+        event PositionEventHandler Click;
+    }
+}
