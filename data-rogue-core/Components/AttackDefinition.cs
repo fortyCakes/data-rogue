4c8b0648a501be3adb47b467aa61fa152c180fed
fortyCakes <alex.j.stuart@gmail.com>
2019-05-02 17:38:42 +0100

    Start on simplifying skill lua

---
+++ data-rogue-core/Components/AttackDefinition.cs
@@ -0,0 +1,13 @@
+﻿using data_rogue_core.EntityEngineSystem;
+
+namespace data_rogue_core.Components
+{
+    public class AttackDefinition : IEntityComponent
+    {
+        public string AttackClass;
+        public bool SpendTime;
+        public string Damage;
+        public int? Accuracy;
+        public string AttackName;
+    }
+}
