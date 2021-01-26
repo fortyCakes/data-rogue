f32e5cc5fdc414943d1b84fbf63362dd9022abac
Alex Stuart <alex.j.stuart@gmail.com>
2021-01-26 20:41:09 +0000

    Show when defences are broken

--- data-rogue-one/IOSystems/BLTDefencesDisplayer.cs
+++ data-rogue-one/IOSystems/BLTDefencesDisplayer.cs
@@ -35,29 +35,44 @@ namespace data_rogue_one.IOSystems
             var aegis = Math.Floor(systemContainer.EventSystem.GetStat(player, "Aegis"));
             var aegisText = $"{currentAegis}/{aegis}";
             var renderAegis = aegis > 0;
+            var tiltFighter = player.Get<TiltFighter>();
+            var brokenTicks = tiltFighter.BrokenTicks;
+            var broken = brokenTicks > 0;
 
-            BLT.Font("");
-            BLT.Layer(BLTLayers.UIElements);
-            RenderSpriteIfSpecified(x, y, spriteManager, "AC", AnimationFrame.Idle0);
-            RenderSpriteIfSpecified(x + 10, y, spriteManager, "SH", AnimationFrame.Idle0);
-            RenderSpriteIfSpecified(x + 20, y, spriteManager, "EV", AnimationFrame.Idle0);
-            if (renderAegis)
+            if (broken)
             {
-                var aegisSprite = currentAegis > 0 ? "aegis" : "aegis_none";
-                RenderSpriteIfSpecified(x + 30, y, spriteManager, aegisSprite, AnimationFrame.Idle0);
-            }            
+                BLT.Font("");
+                BLT.Layer(BLTLayers.UIElements);
+                RenderSpriteIfSpecified(x, y, spriteManager, "defence_broken", AnimationFrame.Idle0);
 
-            BLT.Layer(BLTLayers.Text);
-            BLT.Font("text");
-
-            PrintTextCentered(ac.ToString(), x + 4, y + 2);
-            PrintTextCentered(sh.ToString(), x + 4 + 10, y + 2);
-            PrintTextCentered(ev.ToString(), x + 4 + 20, y + 2);
-            if (renderAegis)
-            {
-                PrintTextCentered(aegisText, x + 4 + 30, y + 2);
+                BLT.Layer(BLTLayers.Text);
+                BLT.Font("text");
+                BLT.Print(x + 10, y + 2, $"DEFENCE BROKEN (Recovery: {100 - brokenTicks / 200}%)");
             }
+            else
+            {
+                BLT.Font("");
+                BLT.Layer(BLTLayers.UIElements);
+                RenderSpriteIfSpecified(x, y, spriteManager, "AC", AnimationFrame.Idle0);
+                RenderSpriteIfSpecified(x + 10, y, spriteManager, "SH", AnimationFrame.Idle0);
+                RenderSpriteIfSpecified(x + 20, y, spriteManager, "EV", AnimationFrame.Idle0);
+                if (renderAegis)
+                {
+                    var aegisSprite = currentAegis > 0 ? "aegis" : "aegis_none";
+                    RenderSpriteIfSpecified(x + 30, y, spriteManager, aegisSprite, AnimationFrame.Idle0);
+                }
 
+                BLT.Layer(BLTLayers.Text);
+                BLT.Font("text");
+
+                PrintTextCentered(ac.ToString(), x + 4, y + 2);
+                PrintTextCentered(sh.ToString(), x + 4 + 10, y + 2);
+                PrintTextCentered(ev.ToString(), x + 4 + 20, y + 2);
+                if (renderAegis)
+                {
+                    PrintTextCentered(aegisText, x + 4 + 30, y + 2);
+                }
+            }
         }
 
         private void PrintTextCentered(string text, int x, int y)
