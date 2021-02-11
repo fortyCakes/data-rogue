3582827ceed6571b125cb68b49e3cb1006fd323d
Alex Stuart <alex.j.stuart@gmail.com>
2021-02-11 19:41:14 +0000

    Start adding map tests.

---
+++ data-rogue-core.UnitTests/Maps/MapTests.cs
@@ -0,0 +1,56 @@
+﻿using data_rogue_core.Components;
+using data_rogue_core.EntityEngineSystem;
+using data_rogue_core.Maps;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_rogue_core.UnitTests.Maps
+{
+    [TestFixture]
+    public class MapTests
+    {
+        private IEntity _defaultCell;
+        private IEntity _voidCell;
+        private IEntity _floorCell;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var physical = new Physical { };
+            var appearance = new Appearance { };
+
+            _defaultCell = new Entity(0, "defaultCell", new IEntityComponent[] { physical, appearance });
+            _voidCell = new Entity(0, "voidCell", new IEntityComponent[] { physical, appearance });
+            _floorCell = new Entity(0, "floorCell", new IEntityComponent[] { physical, appearance });
+        }
+
+        [Test]
+        public void SubMap_OffCentre_PlacesCorrectly()
+        {
+            var map = new Map("Main", _defaultCell);
+            var subMap = new Map("Vault", _voidCell);
+
+            subMap.SetCell(3, 3, _floorCell);
+            subMap.SetCell(4, 4, _floorCell);
+            subMap.SetCell(5, 5, _floorCell);
+            subMap.SetCell(3, 5, _floorCell);
+            subMap.MapGenCommands.Add(new MapGenCommand { MapGenCommandType = MapGenCommandType.Entity, Parameters = "Test", Vector = new Vector(3, 5) });
+
+            map.PlaceSubMap(new MapCoordinate("Main", 10, 10), subMap);
+
+            map.CellAt(10, 10).Should().Be(_floorCell);
+            map.CellAt(11, 11).Should().Be(_floorCell);
+            map.CellAt(12, 12).Should().Be(_floorCell);
+            map.CellAt(10, 12).Should().Be(_floorCell);
+
+            map.MapGenCommands.Single().Vector.Should().Be(new Vector(10, 12));
+
+        }
+    }
+}
